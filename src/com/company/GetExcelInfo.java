93cea6b7cfb771eef10b700da25084b9da21ef57
Jiawei Dai <jdai13@illinois.edu>
2018-06-09 11:00:59 -0500

    Initial commit

---
+++ src/com/company/GetExcelInfo.java
@@ -0,0 +1,46 @@
+package com.company;
+//package jxl.zhanhj;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+public class GetExcelInfo {
+    public static void main(String[] args) {
+	// write your code here
+        GetExcelInfo obj = new GetExcelInfo();
+        File file = new File("myFile.xls");
+        obj.readExcel(file);
+    }
+    public void readExcel(File file) {
+        try {
+            // 创建输入流，读取Excel
+            InputStream is = new FileInputStream(file.getAbsolutePath());
+            // jxl提供的Workbook类
+            Workbook wb = Workbook.getWorkbook(is);
+            // Excel的页签数量
+            int sheet_size = wb.getNumberOfSheets();
+            for (int index = 0; index < sheet_size; index++) {
+                // 每个页签创建一个Sheet对象
+                Sheet sheet = wb.getSheet(index);
+                // sheet.getRows()返回该页的总行数
+                for (int i = 0; i < sheet.getRows(); i++) {
+                    // sheet.getColumns()返回该页的总列数
+                    for (int j = 0; j < sheet.getColumns(); j++) {
+                        String cellinfo = sheet.getCell(j, i).getContents();
+                        System.out.println(cellinfo);
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (BiffException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
